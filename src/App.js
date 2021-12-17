806e920c1cdf6af5ddb00cb20faff18fb8eca427
Nitin Kumar Patel <niit.patel@gmail.com>
2021-12-17 15:09:35 -0600

    appint

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
+
+export default function App() {
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+      <section className="container">
+
+        <h4>{people.length} appointments today</h4>
+
+        <List people={people} />
+        <button onClick={() =>
+          setPeople([])}>
+          clear
+        </button>
+      </section>
+
+    </main>
+  )
+}
